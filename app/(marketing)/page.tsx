6398c930fcb99c23d9d46d9ce819fd59ea4817f8
leothefrench <104638709+leothefrench@users.noreply.github.com>
2024-01-28 13:21:31 +0100

    Marketing file done

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,12 +1,37 @@
-import { Medal } from "lucide-react"
-
-import { Button } from '@/components/ui/button';
 import Link from "next/link";
+import localFont from 'next/font/local';
+import { Poppins } from "next/font/google";
+import { Medal } from "lucide-react";
+
+import { cn } from "@/lib/utils"
+import { Button } from '@/components/ui/button';
+
+const headingFont = localFont({
+    src: "../../public/fonts/font.woff2",
+})
+
+const textFont = Poppins({
+    subsets: ['latin'],
+    weight: [
+        '100',
+        '200',
+        '300',
+        '400',
+        '500',
+        '600',
+        '700',
+        '800',
+        '900'
+    ]
+})
 
 const MarketingPage = () => {
     return (
         <div className="flex items-center justify-center flex-col">
-            <div className="flex justify-center items-center flex-col">
+            <div className={cn(
+                "flex justify-center items-center flex-col",
+                headingFont.className,
+                )}>
                 <div className="mb-4 flex items-center border shadow-sm p-4 bg-amber-100 text-amber-700 rounded-full uppercase">
                     <Medal className="h-6 w-6 mr-2"/>
                     No 1 task managment
@@ -16,7 +41,9 @@ const MarketingPage = () => {
                     work forward
                 </div>
             </div>
-            <div className="text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto">
+            <div className={cn(
+                "text-sm md:text-xl text-neutral-400 mt-4 max-w-xs md:max-w-2xl text-center mx-auto",
+                textFont.className)}>
                 Collaborate, manage project, and reach new productivity peaks. From high rises to the office, the way your team works is unique - accomplish it all with Tasktify
             </div>
             <Button className="mt-6" size="lg" asChild>
